bbfdb15289a2c844360f69d190308c246848aff2
SerGit <sergey-net.com86@mail.ru>
2024-02-21 07:18:43 +0300

    init commit 4-21.02.24 level 3 less 1 1/2

---
+++ src/main/java/ru/Artem_Vorov/level3/lesson1/L3L1_Main.java
@@ -0,0 +1,48 @@
+package ru.Artem_Vorov.level3.lesson1;
+
+import java.util.*;
+
+public class L3L1_Main {
+
+    /*  - Iterable
+            - Collection
+                - List
+                - Set
+                - Queue
+
+        - Map */
+    public static void main(String[] args) {
+        Object[] arr1 = new Object[5];
+        arr1[0] = 1;
+        arr1[1] = "2";
+        arr1[2] = null;
+        arr1[3] = false;
+        arr1[4] = 'X';
+        System.out.println(Arrays.toString(arr1));
+
+        String[] arr2 = new String[3];
+        arr2[0] = "Hello";
+        System.out.println(Arrays.toString(arr2));
+
+        List<String> list = new ArrayList<>();
+        list.add("str1");
+        list.add("str2");
+        list.add("str2");
+        list.add("str3");
+        System.out.println("List: " + list);
+
+        Set<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(2);
+        set.add(3);
+        System.out.println("Set: " + set);
+
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "str");
+        map.put(2, "str");
+        map.put(3, "str2");
+        map.put(3, "str3");
+        System.out.println("Map: " + map);
+    }
+}
