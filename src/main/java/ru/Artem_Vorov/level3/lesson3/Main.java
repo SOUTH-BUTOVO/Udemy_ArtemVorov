050fe8490126bcce47c6d6ad806b6396efb5903c
SerGit <sergey-net.com86@mail.ru>
2024-02-23 07:27:30 +0300

    init commit 5-22.02.24 level 3 less 1 1/2 level 3, less1, hw 1/2 level 3, less2 1/4 level 3, less2 reload level 3, less3

---
+++ src/main/java/ru/Artem_Vorov/level3/lesson3/Main.java
@@ -0,0 +1,31 @@
+package ru.Artem_Vorov.level3.lesson3;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list1 = new ArrayList<>(); // Integer[] array = new Integer[?];
+        list1.add(5);                            // array[0] = 5;
+        list1.add(7);                            // array[1] = 7;
+        list1.add(2, 9);            // array[2] = 9;
+        list1.set(1, 2);                         // array[1] = 2;
+        System.out.print(list1.get(0) + " | ");        // System.out.println(array[0]);
+        System.out.println(list1.size());        // System.out.println(array.length);
+
+        for (int i = 0; i < list1.size(); i++) {
+            System.out.println(list1.get(i));
+        }
+
+        List<Integer> list2 = new LinkedList<>(); // Integer[] array = new Integer[?];
+        list2.add(5);                            // array[0] = 5;
+        list2.add(7);                            // array[1] = 7;
+        list2.add(2, 9);            // array[2] = 9;
+        list2.set(1, 2);                         // array[1] = 2;
+        System.out.print(list2.get(0) + " | ");        // System.out.println(array[0]);
+        System.out.println(list2.size());        // System.out.println(array.length);
+
+        for (Integer integer : list2) {
+            System.out.println(integer);
+        }
+    }
+}
