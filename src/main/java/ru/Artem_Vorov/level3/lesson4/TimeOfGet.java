e02598db651eca9dae06b2077c7880fb90faa56d
SerGit <sergey-net.com86@mail.ru>
2024-02-26 12:57:09 +0300

    init commit 5-22.02.24 level 3 less 1 1/2 level 3, less1, hw 1/2 level 3, less2 1/4 level 3, less2 reload level 3, less3, HW. level 3, less5 1/2-2

--- src/main/java/ru/Artem_Vorov/level3/lesson4/TimeOfGet.java
+++ src/main/java/ru/Artem_Vorov/level3/lesson4/TimeOfGet.java
@@ -1,4 +1,36 @@
 package ru.Artem_Vorov.level3.lesson4;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
 public class TimeOfGet {
+    public static void main(String[] args) {
+        System.out.println(getTimeMsOfGet(fill(new ArrayList())));
+        System.err.println(getTimeMsOfGet(fill(new LinkedList())));
+    }
+
+    private static List fill(List list) {
+        for (int i = 0; i < 100000; i++) {
+            list.add(new Object());
+        }
+        return list;
+    }
+
+    static long getTimeMsOfGet(List list) {
+        Date startTime = new Date();
+        get100000(list);
+        Date endTime = new Date();
+        long fin = endTime.getTime() - startTime.getTime();
+        return fin;
+    }
+
+    static void get100000(List list) {
+        if (list.isEmpty()) return;
+        int x = list.size() / 2;
+        for (int i = 0; i < 100000; i++) {
+            list.get(x);
+        }
+    }
 }
