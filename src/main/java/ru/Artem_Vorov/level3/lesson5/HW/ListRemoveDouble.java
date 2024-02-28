df431358e9eec8858a28438e009d8eea0ca5367e
SerGit <sergey-net.com86@mail.ru>
2024-02-28 10:09:46 +0300

    init commit 5-22.02.24 level 3 less 1 1/2 level 3, less1, hw 1/2 level 3, less2 1/4 level 3, less2 reload level 3, less3, HW. level 3, less5 1 level 3, less5, HW 1/2

---
+++ src/main/java/ru/Artem_Vorov/level3/lesson5/HW/ListRemoveDouble.java
@@ -0,0 +1,47 @@
+package ru.Artem_Vorov.level3.lesson5.HW;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListRemoveDouble {
+    static void fix(List<String> list) {
+        //List<String> list = List.of("лира", "лоза", "роза");
+        String r = "р";   // - удалять из списка строк все слова, содержащие букву «р»
+        char l = 'л';   // - удваивать все слова содержащие букву «л»
+        // - если слово содержит и букву «р» и букву «л», то оставить это слово без изменений.
+
+//        for (int i = 0; i < 1; i++) {
+//            boolean index = list.contains(r);
+//
+//            if (index) {
+//                System.out.println("Символ есть в листе");
+//            } else {
+//                System.out.println("Символ не найден");
+//            }
+//        }
+//        System.out.println(list);
+
+        List<String> newList = new ArrayList<>();
+        for (String word : list) {
+            if (word.contains("р") && word.contains("л")) {
+                newList.add(word);
+//                System.out.println ("Буква" + " (р) " +
+//                        "была найдена в слове " + "(" + word + ")" + " и слово будет удалено.");
+//                list.remove(word);
+            }
+            if (word.contains("л") && !word.contains("р")) {
+                newList.add(word);
+                newList.add(word);
+            }
+
+        }
+        //list.clear();
+        //list.addAll(newList);
+
+        list = newList;
+        System.out.println(list);
+        System.out.println(newList);
+    }
+}
--- src/main/java/ru/Artem_Vorov/level3/lesson5/HW/Main.java
+++ src/main/java/ru/Artem_Vorov/level3/lesson5/HW/Main.java
@@ -3,26 +3,27 @@ package ru.Artem_Vorov.level3.lesson5.HW;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException, IOException {
 
         /* 1. Посчитать время работы метода set в LinkedList и ArrayList
         Посчитать время работы метода remove в LinkedList и ArrayList */
-        System.out.println(TimeOfSet.getTimeMsOfSet(TimeOfSet.fill(new ArrayList())));
-        System.err.println(TimeOfSet.getTimeMsOfSet(TimeOfSet.fill(new LinkedList())));
-        Thread.sleep(500);
-        System.out.println("----------");
-        System.out.println(TimeOfRemove.getTimeMsOfRemove(TimeOfRemove.fill(new ArrayList())));
-        System.err.println(TimeOfRemove.getTimeMsOfRemove(TimeOfRemove.fill(new LinkedList())));
-        Thread.sleep(500);
-        System.out.println("----------");
+//            System.out.println(TimeOfSet.getTimeMsOfSet(TimeOfSet.fill(new ArrayList())));
+//            System.err.println(TimeOfSet.getTimeMsOfSet(TimeOfSet.fill(new LinkedList())));
+//            Thread.sleep(500);
+//            System.out.println("----------");
+//            System.out.println(TimeOfRemove.getTimeMsOfRemove(TimeOfRemove.fill(new ArrayList())));
+//            System.err.println(TimeOfRemove.getTimeMsOfRemove(TimeOfRemove.fill(new LinkedList())));
+//            Thread.sleep(500);
+//            System.out.println("----------");
 
         /* 2. Создай список строк. Добавь в него 5 строчек с клавиатуры. Удали последнюю строку и
         вставь её в начало. Повторить 5 раз. Используя цикл выведи содержимое на экран,
         каждое значение с новой строки.
         Удали 3ий элемент списка, и выведи оставшиеся элементы в обратном порядке. */
-        ListRemove.listStr();
+        //ListRemove.listStr();
         System.out.println("\n" + "----------");
 
         /* 4. Создай список слов, заполни его самостоятельно. Метод fix должен:
@@ -38,6 +39,9 @@ public class Main {
         лира
         лоза
         лоза */
+        ListRemoveDouble.fix(List.of("лира", "лоза", "роза", "стол", "кровать", "люстра"));
+        //ListRemoveDouble.fix();
+        System.out.println("\n" + "----------");
 
 
         /* 5. Введи с клавиатуры 5 слов в список строк. Метод doubleValues должен удваивать слова по принципу:
