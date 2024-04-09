50eda9737bee46bc2ee68f1e746d59e56dab4c28
SerGit <sergey-net.com86@mail.ru>
2024-04-09 18:44:05 +0300

    init commit 09.04.24 level 5, less1, HW 2/3

--- src/main/java/ru/Artem_Vorov/level5_Java_core/lesson1/HW/Binary.java
+++ src/main/java/ru/Artem_Vorov/level5_Java_core/lesson1/HW/Binary.java
@@ -5,8 +5,28 @@ package ru.Artem_Vorov.level5_Java_core.lesson1.HW;
 public class Binary {
 
     void binarySearch() {
-        int[] mass = {1, 1, 2, 3, 4, 10};
-        int valueToFind = 3;
+        int[] mass = {0, 1, 1, 2, 3, 4, 7, 10, 12, 13, 15};
+        int valueToFind = 10;
 
+        int low = 0;
+        int high = mass.length-1;
+        int index = -1;
+        int midMass;
+
+        while (low <= high) {
+            midMass = low + (high - low) / 2;
+            if (mass[midMass] < valueToFind) {
+                low = midMass + 1;
+            } else if (mass[midMass] > valueToFind) {
+                high = midMass - 1;
+                //break;
+            } else {
+                index = midMass;
+                //System.out.println("Число: " + valueToFind + ", в массиве не найдено.");
+                break;
+            }
+        }
+        System.out.println((index + 1) + " search num");
+        System.out.println(mass.length + " length mass");
     }
 }
--- src/main/java/ru/Artem_Vorov/level5_Java_core/lesson1/HW/Main.java
+++ src/main/java/ru/Artem_Vorov/level5_Java_core/lesson1/HW/Main.java
@@ -6,9 +6,11 @@ public class Main {
     public static void main(String[] args) {
         /* 1. Задача по алгоритмам. Найти ряд Фибоначчи для числа 10 */
         Fibonacci fibonacci = new Fibonacci();
-        fibonacci.fi();
+        //fibonacci.fi();
 
         /* 2. Задача по алгоритмам. Реализовать алгоритм бинарного поиска (загугли, разберись что это и реализуй) */
+        Binary binary = new Binary();
+        binary.binarySearch();
 
         /* 3. Ввести с клавиатуры в список 5 слов. Нужно подсчитать количество одинаковых слов в списке.
         Результат нужно представить в виде словаря Map<String, Integer>, где первый параметр – уникальная строка,
