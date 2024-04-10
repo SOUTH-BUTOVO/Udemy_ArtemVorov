d29f22fe6a85926da450a656301e9aa6205048b4
SerGit <sergey-net.com86@mail.ru>
2024-04-10 22:33:03 +0300

    init commit 09.04.24 level 5, less1, HW 2.1/3

--- src/main/java/ru/Artem_Vorov/level5_Java_core/lesson1/HW/Binary.java
+++ src/main/java/ru/Artem_Vorov/level5_Java_core/lesson1/HW/Binary.java
@@ -2,31 +2,38 @@ package ru.Artem_Vorov.level5_Java_core.lesson1.HW;
 
 /* 2. Задача по алгоритмам. Реализовать алгоритм бинарного поиска (загугли, разберись что это и реализуй) */
 
+import java.util.Arrays;
+
 public class Binary {
 
     void binarySearch() {
         int[] mass = {0, 1, 1, 2, 3, 4, 7, 10, 12, 13, 15};
-        int valueToFind = 10;
+        Arrays.sort(mass);
+        int valueToFind = 5;
 
-        int low = 0;
-        int high = mass.length-1;
-        int index = -1;
-        int midMass;
+        int left = 0;
+        int right = mass.length - 1;
+        int index = 0;
+        int massMid;
 
-        while (low <= high) {
-            midMass = low + (high - low) / 2;
-            if (mass[midMass] < valueToFind) {
-                low = midMass + 1;
-            } else if (mass[midMass] > valueToFind) {
-                high = midMass - 1;
+        while (left <= right) {
+            if (left ==-1) {
+                System.out.println("Число: " + valueToFind + ", в массиве не найдено.");
+            break;
+            }
+            massMid = left + (right - left) / 2;
+            if (mass[massMid] < valueToFind) {
+                left = massMid + 1;
+            } else if (mass[massMid] > valueToFind) {
+                right = massMid - 1;
                 //break;
             } else {
-                index = midMass;
+                index = massMid;
                 //System.out.println("Число: " + valueToFind + ", в массиве не найдено.");
                 break;
             }
         }
-        System.out.println((index + 1) + " search num");
+        System.out.println("Искомое число: " + mass[index] + ", найдено в массиве под №: " + (index + 1));
         System.out.println(mass.length + " length mass");
     }
 }
---
+++ src/main/java/ru/Artem_Vorov/level5_Java_core/lesson1/HW/IdenticalWords.java
@@ -0,0 +1,14 @@
+package ru.Artem_Vorov.level5_Java_core.lesson1.HW;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class IdenticalWords {
+    /* 3. Ввести с клавиатуры в список 5 слов. Нужно подсчитать количество одинаковых слов в списке.
+        Результат нужно представить в виде словаря Map<String, Integer>, где первый параметр – уникальная строка,
+        а второй – число, сколько раз данная строка встречалась в списке. Вывести содержимое словаря на экран. */
+
+    void dictionaryWords() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
